b70733cb3d1151f68b92f09bb21983d5df98d652
melissay94 <melissay94@yahoo.com>
2020-02-20 16:47:32 -0800

    Added passport for sign in authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,43 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const passport = require("../config/ppConfig");
+const db = require("../models");
 
-router.get('/signup', function(req, res) {
+const router = express.Router();
+
+router.get('/signup', (req, res) => {
   res.render('auth/signup');
 });
 
-router.get('/login', function(req, res) {
+// POST - Sign up new users
+router.post("/signup", (req, res) => {
+  db.user.findOrCreate({
+    where: {
+      email: req.body.email
+    }, defaults: {
+      name: req.body.name,
+      password: req.body.password
+    }
+  }).then(([user, created]) => {
+    if (created) {
+      console.log("User created");
+      res.redirect("/");
+    } else {
+      console.log("User already exists");
+      res.redirect("/auth/signup");
+    }
+  }).catch(err => {
+    console.log("Error occurred creating user", err);
+    res.redirect("/auth/signup");
+  })
+});
+
+router.get('/login', (req, res) => {
   res.render('auth/login');
 });
+
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/auth/login"
+}));
 
 module.exports = router;
--- index.js
+++ index.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 
 const express = require('express');
 const ejsLayouts = require('express-ejs-layouts');
+const session = require("express-session");
+const passport = require("./config/ppConfig");
+
 const authController =  require('./controllers/auth');
 
 const app = express();
@@ -12,6 +15,13 @@ app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
 app.use(ejsLayouts);
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize()); // Has to be after the session use 
+app.use(passport.session());
 
 app.get('/', function(req, res) {
   res.render('index');
